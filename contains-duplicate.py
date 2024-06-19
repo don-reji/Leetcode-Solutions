3a2fa2ca30bfe437fca9948f9e2df9e29d5753e1
Don_Reji <donreji88@gmail.com>
2024-06-19 21:28:30 +0530

    .\contains-duplicate.py

---
+++ contains-duplicate.py
@@ -0,0 +1,28 @@
+# link - https://leetcode.com/problems/contains-duplicate/description/
+# Given an integer array nums, return true if any value appears at least twice in the array,
+# and return false if every element is distinct.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = set()
+        for i in nums:
+            if i in count:
+                return True
+            count.add(i)
+        return False
+
+        # O(n) time and O(n) space, worst case
